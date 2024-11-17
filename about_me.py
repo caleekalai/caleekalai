d8d6542b8eafa60112297539e16441689f5bc105
Anoop Joseph <30518428+caleekalai@users.noreply.github.com>
2024-11-17 19:48:50 +0530

    Add files via upload

---
+++ about_me.py
@@ -0,0 +1,39 @@
+import streamlit as st 
+
+from forms.contact import contact_form 
+
+@st.experimental_dialog("Contact Me")
+def show_contact_form():
+	contact_form()
+
+#------Hero Section----------
+col1, col2 = st.columns(2, gap="small", vertical_alignment="center")
+
+with col1:
+	st.image("./assets/profile_image.png", width=230)
+
+
+with col2:
+	st.title("CaleekalAI",  anchor=False)
+	st.write("Agent of Artificial Intelligence and Generative AI")
+
+	if st.button("Contact Me"):
+		show_contact_form()
+
+
+# -------Tasks------------------
+
+st.subheader("AIAgent Tasks", anchor=false)
+st.write(
+  """
+  	- Youtube videos to Articles
+  	- Toutube Videos Summary
+  	
+
+
+  """
+)
+
+
+
+
